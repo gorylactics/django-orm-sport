4ca4f88c0da79eabe4607ab466cb8476bdfa7bbc
Adrian <adrian.miranda.r@gmail.com>
2021-08-15 18:53:47 -0400

    16 consultas realizadas

--- leagues/views.py
+++ leagues/views.py
@@ -27,11 +27,36 @@ def index(request):
 		'atlantic' : League.objects.filter(name__contains = 'atlantic'),
 		# todas las ligas de la región atlantic
 
-		# 'dallas' : League.objects.filter(name__teams__contains = 'dallas')
-		# 'dallas' : Team.objects.filter(league__name__contains='Dallas'),
-		'dallas' : League.objects.filter(team_name__teams__contains = 'dallas'),
-		# 'dallas': Team.objecst.filter(team_name__contains = 'dallas'),
+
+		'dallas' : Team.objects.filter(location__contains = 'dallas'),
 		# todos los equipos con sede en Dallas
+
+		'raptors' : Team.objects.filter(team_name__contains = 'raptors'),
+		# todos los equipos nombraron los Raptors
+
+		'city' : Team.objects.filter(location__contains = 'city'),
+		# todos los equipos cuya ubicación incluye "Ciudad"
+
+		'T' : Team.objects.filter(team_name__startswith = 't'),
+		# todos los equipos cuyos nombres comienzan con "T"
+
+		'ubicacion' : Team.objects.all().order_by('location'),
+		# todos los equipos, ordenados alfabéticamente por ubicación
+
+		'inverso' : Team.objects.all().order_by('-team_name'),
+		# todos los equipos, ordenados por nombre de equipo en orden alfabético inverso
+
+		'cooper' : Player.objects.filter(last_name__startswith = 'cooper'),
+		# cada jugador con apellido "Cooper"
+		'joshua' : Player.objects.filter(first_name__startswith = 'joshua'),
+		# cada jugador con nombre "Joshua"
+
+		'joshuaCooper': Player.objects.filter(last_name__startswith = 'cooper').exclude(first_name = 'joshua'), 
+		# todos los jugadores con el apellido "Cooper" EXCEPTO aquellos con "Joshua" como primer nombre
+
+		'AlexanderWyatt' : Player.objects.filter(first_name__contains = 'alexander') | Player.objects.filter(first_name__contains = 'Wyatt')
+		# todos los jugadores con nombre "Alexander" O nombre "Wyatt"
+
 
 	}
 	return render(request, "leagues/index.html", context)
